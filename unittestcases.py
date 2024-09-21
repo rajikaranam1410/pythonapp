fe8cbf190fa3b90a344f403b3fffeaf098df2084
rajikaranam1410 <rajikaranam1410@gmail.com>
2024-09-21 11:26:16 +0530

    Update unittestcases.py

--- unittestcases.py
+++ unittestcases.py
@@ -1,5 +1,5 @@
 import unittest
-from multiply import multipliaction
+from multiply import multiplication
 class MultiplyTestCase(unittest.TestCase):
     def test_1(self):
         result = multiply(3, 4)
